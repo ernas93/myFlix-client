a2250fb8f47b78e405669ca1a644dc802609536f
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-05-24 19:56:09 +0200

    added Navbar and links

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,6 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
+import { NavigationBar } from "../navigation-bar/navigation-bar";
 import Row from "react-bootstrap/Row";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -51,6 +52,13 @@ export const MainView = () => {
 
     return (
         <BrowserRouter>
+            <NavigationBar
+            user={user}
+            onLoggedOut={() => {
+                setUser(null);
+                setToken(null)
+            }}
+            />
             <Row className="justify-content-md-center">
                 <Routes>
                     <Route
